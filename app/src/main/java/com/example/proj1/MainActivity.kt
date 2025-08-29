d98e0198b7f4027f7e7c06960e855bb29e2a8107
alvinscd23-png <alvins.cd23@bmsce.ac.in>
2025-08-29 12:30:01 +0530

    added the calucator functio

--- app/src/main/java/com/example/proj1/MainActivity.kt
+++ app/src/main/java/com/example/proj1/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.proj1
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,6 +14,69 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activit_main_2)
         Toast.makeText(this, "Welcome World", Toast.LENGTH_LONG).show()
+        val btnAdd = findViewById<Button>(R.id.btnAdd);
+        val btnSub = findViewById<Button>(R.id.btnSub);
+        val btnMul = findViewById<Button>(R.id.btnMul);
+        val btnDiv = findViewById<Button>(R.id.btnDiv);
+        val etA = findViewById<EditText>(R.id.et_a);
+        val etB = findViewById<EditText>(R.id.et_b);
+        val resultView = findViewById<TextView>(R.id.ans);
+        btnAdd.setOnClickListener{
+            val aText = etA.text.toString()
+            val bText = etB.text.toString()
+
+            val a = aText.toDoubleOrNull()
+            val b = bText.toDoubleOrNull()
+
+            if (a != null && b != null) {
+                val sum = a + b
+                resultView.text = sum.toString()
+            } else {
+                resultView.text = "Invalid input"
+            }
+        }
+        btnSub.setOnClickListener{
+            val aText = etA.text.toString()
+            val bText = etB.text.toString()
+
+            val a = aText.toDoubleOrNull()
+            val b = bText.toDoubleOrNull()
+
+            if (a != null && b != null) {
+                val sub = a - b
+                resultView.text = sub.toString()
+            } else {
+                resultView.text = "Invalid input"
+            }
+        }
+        btnMul.setOnClickListener{
+            val aText = etA.text.toString()
+            val bText = etB.text.toString()
+
+            val a = aText.toDoubleOrNull()
+            val b = bText.toDoubleOrNull()
+
+            if (a != null && b != null) {
+                val mul = a * b
+                resultView.text = mul.toString()
+            } else {
+                resultView.text = "Invalid input"
+            }
+        }
+        btnDiv.setOnClickListener{
+            val aText = etA.text.toString()
+            val bText = etB.text.toString()
+
+            val a = aText.toDoubleOrNull()
+            val b = bText.toDoubleOrNull()
+
+            if (a != null && b != null) {
+                val div = a / b
+                resultView.text = div.toString()
+            } else {
+                resultView.text = "Invalid input"
+            }
+        }
     }
 
     override fun onStart() {
